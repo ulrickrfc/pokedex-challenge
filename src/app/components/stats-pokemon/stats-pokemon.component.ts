70a4cde1a8d9c35823d9d1f3388a26fae5916e37
Ulrick Fiaes <ulrickroque@gmail.com>
2022-05-10 11:17:36 -0300

    fix: line stats progress bar with the greater value for each stat

--- src/app/components/stats-pokemon/stats-pokemon.component.ts
+++ src/app/components/stats-pokemon/stats-pokemon.component.ts
@@ -17,4 +17,19 @@ export class StatsPokemonComponent implements OnInit {
 
     return fixed.replace('-', ' ');
   }
+  fixLineStats(value: string, statName: string) {
+    if (statName === 'hp') {
+      return (100 * Number(value)) / 255;
+    } else if (statName === 'attack') {
+      return (100 * Number(value)) / 190;
+    } else if (statName === 'defense' || statName === 'special-defense') {
+      return (100 * Number(value)) / 230;
+    } else if (statName === 'special-attack') {
+      return (100 * Number(value)) / 194;
+    } else if (statName === 'speed') {
+      return (100 * Number(value)) / 180;
+    }
+
+    return 0;
+  }
 }
