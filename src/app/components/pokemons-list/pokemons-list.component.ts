acaaf54bd85aecac9146d4429e5e9b6d0c719725
Ulrick Fiaes <ulrickroque@gmail.com>
2022-06-29 22:02:57 -0300

    chore: removing console log

--- src/app/components/pokemons-list/pokemons-list.component.ts
+++ src/app/components/pokemons-list/pokemons-list.component.ts
@@ -60,7 +60,6 @@ export class PokemonsListComponent implements OnInit {
             ...response.results.slice(-12),
           ])
       );
-    console.log(this.pokemons.results);
   }
   backToTop() {
     document.body.scrollTop = 0;
